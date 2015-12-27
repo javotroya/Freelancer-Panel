e977caa255ff57a9fcc4634193b7c33440c07598
Javier Troya <jtroya@rootstack.com>
2015-12-27 08:30:17 -0500

    Configuration file.

---
+++ freelancer-panel.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Freelancer Panel
+Plugin URI: https://github.com/javiertroya/Freelancer-Panel
+Description: Temporary description.
+Author: Javo Troya
+Version: 1.0
+Author URI: https://github.com/javiertroya/
+*/
